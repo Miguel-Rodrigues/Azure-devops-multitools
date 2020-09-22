2203b7821b21302e0a3294690f91739a1e56ab91
Miguel Rodrigues (Externo) <miguel.rodrigues.Externo@ageas.pt>
2020-09-22 22:29:29 +0100

    Complete tfdestroy script

--- Scripts/Devops Maintenance/TFS Purge Tool/tfdestroy.ps1
+++ Scripts/Devops Maintenance/TFS Purge Tool/tfdestroy.ps1
@@ -1,70 +1,126 @@
+## TFDestroy - deletes and erases projects and it contents.
+
+## TODO: Erase git repositories
+##       Trigger database shrink
+##       Maintenance steps (detach project, backup reattach)
+##       Check if project and sources exist before erasing, to avoid errors using TF Tool
+
+## WARNING!! This software is available "AS IS" with no liability or warranty.
+## Be aware this is a very destructive tool!! Once executed the actions cannot be undone. Create a backup before using it.
+## Read the code before using it and use the "-WhatIf" switch to simulate the execution. Do not EVER use this tool on a production directly!
+## I'm not responsible for any your consequences by the misusage using this tool. You've been warned. :)
+
+## Usage: 
+## .\tfdestroy.ps1 -Server "http://localhost:8080/devops" -Collection "JohnDoe" -ProjectMames @("Project 1","Project 2","Project 3") -PurgeDeleted
+
 [CmdletBinding()]
 param(    
     # Server URI
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string] $Server,
 
     # Collection Name
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string] $CollectionName,
 
-    # PAT (Personal Access Token) for authentication
-    [Parameter(Mandatory=$true)]
-    [string] $AccessToken,
-
     # List of projects to delete
-    [array] $ProjectMames,
+    [array] $ProjectMames = @(),
+
+    # Purges already deleted files (required if there are projects listed in -ProjectMames parameter)
+    [switch] $PurgeDeleted,
+    
+    # Enables "/preview" flag on TF destroy command, that executes the prompt but with no actions applied.
+    [switch] $WhatIf,
+
+    # Skips project validation (used on TFS 2017 or earlier for retrocompatibility)
+    [switch] $SkipVerify,
 
     # Team Foundation CLI Client path (Default: Visual studio 2019 Community)
-    [string] $TFClientPath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\tf.exe",
-    
-    # enables "/preview" flag on TF command
-    [switch] $WhatIf = $False
+    [string] $TFClientPath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer"
 )
 
-Write-Host "Validating project existence"
-$headers = @{
-    Authorization = "Basic $AccessToken"
-}
+try {
+    if (-not $SkipVerify) {
+        Write-Host "Validating existing projects..."
+        $deletedProjects = Invoke-RestMethod -UseDefaultCredentials -Method "GET" -Uri "$Server/$CollectionName/_apis/projects?stateFilter=deleted&api-version=5.0"
 
-# $deletedProjects = Invoke-RestMethod -Method "GET" -Headers $headers -Uri "http://$Server/$CollectionName/_apis/projects?stateFilter=deleted&api-version=5.0-preview.1"
+        [array] $projectsToPurge = @();
 
-# if (-not [Array]::Empty($existingProjects)) {
-#     $existingProjects = Invoke-RestMethod -Method "GET" -Headers $headers -Uri "http://$Server/$CollectionName/_apis/projects?stateFilter=wellFormed&api-version=5.0-preview.1"
-# }
+        if ($ProjectMames.Length -eq 0) {
+            $projectsToPurge = $deletedProjects.value
+        }
+        else {
+            $wellFormedProjects = Invoke-RestMethod -UseDefaultCredentials -Method "GET" -Uri "$Server/$CollectionName/_apis/projects?stateFilter=wellFormed&api-version=5.0"
 
-$defaultOption = 1
-$options = @(
-    New-Object System.Management.Automation.Host.ChoiceDescription "&Delete","Deletes all refered projects"
-    New-Object System.Management.Automation.Host.ChoiceDescription "&Cancel","Cancels the operation"
-)
+            $projectsToPurge = $deletedProjects.value + $wellFormedProjects.value
 
-Write-Warning "The following projects will be permanently deleted."
+            if ($PurgeDeleted) {
+                $projectsToPurge = ($projectsToPurge | Where-Object { $_.name -in $ProjectMames -or $_.state -eq "Deleted" })
+            }
+            else {
+                $projectsToPurge = ($projectsToPurge | Where-Object { $_.name -in $ProjectMames })
+            }
+        }
 
-$projectsToPurge | ForEach-Object {
-    Write-Warning "${ $projectsToPurge.IndexOf($_) }: $_.Name - $_.State"
-  }
+        $projectsToPurge = $projectsToPurge | Select-Object { $_.name }
+    }   
+    else {
+        $projectsToPurge = $ProjectMames
+    }
+
+    if ($projectsToPurge.Length -eq 0) {
+        $message = "There are no projects to be deleted."
+        if (-not $PurgeDeleted -and -not$ProjectMames.Length -eq 0) {
+            $message += " Apply -purgeDeleted flag to purge already deleted projects."
+        }
+
+        throw $message
+    }
 
 
-$selectedOption = $Host.UI.PromptForChoice("TFS Purge Tool", "The operation cannot be undone. Proceed?", $options, $defaultOption))
-if ($selectedOption -eq 0) {
-    Write-Host "Purging TFS projects in ""$CollectionName"" collection database..."
+    $defaultOption = 1
+    $options = @(
+        New-Object System.Management.Automation.Host.ChoiceDescription "&Delete","Deletes all refered projects"
+        New-Object System.Management.Automation.Host.ChoiceDescription "&Cancel","Cancels the operation"
+    )
 
-    $command = "$TFClientPath destroy /i /startcleanup /collection $Server/$collectionName /loginType:OAuth /login:.,$AccessToken)"
-
-    $projectsToPurge | foreach-object {
-        "$$/$_"
+    [string] $projectList
+    $projectsToPurge | ForEach-Object {
+        $projectList += "`t`t$($projectsToPurge.IndexOf( $_ )): $_`n"
     }
     
-    if ($WhatIf) {
-        Write-Information "WhatIf flag enabled. This is a preview only. No content will be deleted."
-        $command += " /preview"
+    Write-Warning "The following projects will be permanently deleted.`n$projectList"
+
+
+    $selectedOption = $Host.UI.PromptForChoice("TFS Purge Tool", "The operation cannot be undone. Proceed?", $options, $defaultOption)
+    if ($selectedOption -eq 0) {
+        Write-Host "Purging TFS projects in ""$CollectionName"" collection database..."
+
+        $projectList = [string]::Empty
+
+        if ($WhatIf) {
+            Write-Host "WhatIf flag enabled. This is a preview only. No content will be deleted."
+        }
+
+        $projectsToPurge | foreach-object {
+            Write-Host "Deleting `"$CollectionName/$_`"..."
+
+            if ($WhatIf) {
+                & "$TFClientPath\TF.exe" destroy """`$/$_""" /i /startcleanup /s:"$Server/$CollectionName" /preview
+            }
+            else {
+                & "$TFClientPath\TF.exe" destroy """`$/$_""" /i /startcleanup /s:"$Server/$CollectionName"
+                & "$TFClientPath\TfsDeleteProject.exe" /q /force /collection:"$Server/$CollectionName" """$_"""
+            }
+        }       
+
+        Write-Host "Done."
     }
-
-    & $command
-
-    Write-Host "All projects deleted."
+    else {
+        Write-Host "Operation canceled. Exiting..."
+    }
 }
-else {
-    Write-Host "Operation canceled. Exiting..."
+catch {
+    Write-Error "$_"
+    Write-Error "Execution failed. Check log above."
 }
