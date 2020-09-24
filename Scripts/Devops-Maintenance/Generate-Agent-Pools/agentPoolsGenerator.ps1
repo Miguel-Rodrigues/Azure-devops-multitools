f71880d9fcad0db4cb665c4758b322b84475391e
Miguel Rodrigues (Externo) <miguel.rodrigues.Externo@ageas.pt>
2020-09-24 23:21:21 +0100

    Define default package folder

--- Scripts/Devops-Maintenance/Generate-Agent-Pools/agentPoolsGenerator.ps1
+++ Scripts/Devops-Maintenance/Generate-Agent-Pools/agentPoolsGenerator.ps1
@@ -3,7 +3,10 @@
 param (
     # Configuration file
     [Parameter(Mandatory=$true)]
-    [string] $config
+    [string] $config,
+
+    # Agent Pool Package Directory
+    [string] $AgentPoolPackageDir = "..\..\vsts-agent-win-x64-2.144.2"
 )
 
 function LoadConfig([string] $configLocation) {
@@ -42,7 +45,7 @@ try {
                 New-Item $_.workingDir -ItemType Directory
             }
 
-            Copy-Item -PassThru -Recurse -Path ..\..\vsts-agent-win-x64-2.144.2\* -Destination .\
+            Copy-Item -PassThru -Recurse -Path $AgentPoolPackageDir\* -Destination .\
 
             & $_.agent\config.cmd --url $definition.url --auth pat --token $definition.token --pool $definition.pool `
                 --agent $_.agent --work $_.workingDir  --unattended --runAsService
