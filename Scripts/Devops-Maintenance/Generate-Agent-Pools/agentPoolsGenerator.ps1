10366aa2e01e7fc6928b59d8d82ca534868bb1ca
Miguel Rodrigues (Externo) <miguel.rodrigues.Externo@ageas.pt>
2020-09-24 23:13:32 +0100

    Add AgentPoolGenerator.ps1

---
+++ Scripts/Devops-Maintenance/Generate-Agent-Pools/agentPoolsGenerator.ps1
@@ -0,0 +1,56 @@
+
+[CmdletBinding()]
+param (
+    # Configuration file
+    [Parameter(Mandatory=$true)]
+    [string] $config
+)
+
+function LoadConfig([string] $configLocation) {
+    
+    # Accept jsonc (json with comments)
+    $configFile = Get-Content $ConfigLocation -raw
+    $configFile = $configFile -replace '(?m)(?<=^([^"]|"[^"]*")*)//.*' -replace '(?ms)/\*.*?\*/'
+    $config = $configFile | ConvertFrom-Json
+
+    if ($null -eq $config) {
+        throw [ConfigurationException] "$ConfigLocation was not found is not correctly configured."
+    }
+
+    $config
+}
+
+try {
+    $configObject = LoadConfig $config
+    
+    $configObject | ForEach-Object  {
+        $definition = $_
+
+        if (-not(Test-Path $definition.pool)) {
+            New-Item $definition.pool -ItemType Directory
+        }
+
+        Set-Location $definition.pool
+
+        $_.agents | ForEach-Object {
+
+            if (-not(Test-Path $_.agent)) {
+                New-Item $_.agent -ItemType Directory
+            }
+
+            if (-not(Test-Path $_.workingDir)) {
+                New-Item $_.workingDir -ItemType Directory
+            }
+
+            Write-Host "Copy-Item -PassThru -Recurse -Path ..\..\vsts-agent-win-x64-2.144.2\* -Destination .\"
+
+            Write-Host "& .\'$($_.agent)'\config.cmd --url '$($definition.url)' --auth pat --token '$($definition.token)' --pool '$($definition.pool)' --agent '$($_.agent)' --work '$($_.workingDir)'  --unattended --runAsService"
+        }
+
+        Set-Location ..\
+    }
+}
+catch {
+    Write-Error $_
+    Write-Host "Operation failed, check error above."
+}
